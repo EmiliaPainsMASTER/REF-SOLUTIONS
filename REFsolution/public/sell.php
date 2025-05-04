f11f17281b69b7a3a9b9b452f24648f9aed7332e
Franklin Addo <B00159369@mytudublin.ie>
2025-05-04 01:55:56 +0100

    Update sell.php

--- REFsolution/public/sell.php
+++ REFsolution/public/sell.php
@@ -2,6 +2,12 @@
 session_start();
 require_once '../src/Core/Database/DBconnect.php';
 
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $sellerName = $_POST['firstname'];
